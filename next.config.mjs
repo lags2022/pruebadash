bc46ca135360ce9af04a58150a8b81a5f98b7548
lags2022 <lguzman.58erb@outlook.com>
2024-09-04 23:47:29 -0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,66 @@
+import "./src/env.mjs";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/api/portraits/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cloudflare-ipfs.com",
+        pathname: "/ipfs/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/u/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.amazonaws.com",
+        pathname: "/redqteam.com/isomorphic-furyroad/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "isomorphic-furyroad.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "isomorphic-furyroad.vercel.app",
+      },
+    ],
+  },
+  webpack: (config) => {
+    let modularizeImports = null;
+    config.module.rules.some((rule) =>
+      rule.oneOf?.some((oneOf) => {
+        modularizeImports = oneOf?.use?.options?.nextConfig?.modularizeImports;
+        return modularizeImports;
+      })
+    );
+    if (modularizeImports?.["@headlessui/react"])
+      delete modularizeImports["@headlessui/react"];
+    return config;
+  },
+};
 
 export default nextConfig;
